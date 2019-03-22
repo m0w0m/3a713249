f49d6626c0e43723f674208f91a45a52c35383e5
m0w0m <48341730+m0w0m@users.noreply.github.com>
2019-03-22 09:20:31 +0800

    Add files via upload

---
+++ 作業08.cpp
@@ -0,0 +1,39 @@
+#include "stdio.h"
+#include "stdlib.h"
+int main()
+{
+	int n ,a ;
+	printf("¡PQa?1DR3-O?d3oao?e?A"); 
+	scanf("%d",&a);
+	   for(int i=2;i<=a;i++)
+{
+	 n = 0;
+    for(int j=1;j<=a;j++) 
+        if(i%j==0)
+            n++;
+            if(n==2)
+		        printf("%d\t",i);
+}
+    printf("\n");
+    system("pause");
+    return 0;
+}#include "stdio.h"
+#include "stdlib.h"
+int main()
+{
+	int n ,a ;
+	printf("¡PQa?1DR3-O?d3oao?e?A"); 
+	scanf("%d",&a);
+	   for(int i=2;i<=a;i++)
+{
+	 n = 0;
+    for(int j=1;j<=a;j++) 
+        if(i%j==0)
+            n++;
+            if(n==2)
+		        printf("%d\t",i);
+}
+    printf("\n");
+    system("pause");
+    return 0;
+}
